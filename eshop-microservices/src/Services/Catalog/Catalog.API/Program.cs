dc3be74d768b59ca6eec5f2b96c4f682093300b8
Jagadeesh Subramani <jagadeesh.subramani@versent.com.au>
2024-07-23 13:19:36 +1000

    Added Health check Added third party libraries for doing postgres db health check.

--- eshop-microservices/src/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,7 @@
 
 using Catalog.API.Data;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,15 +19,20 @@ builder.Services.AddMediatR(config =>
 
 builder.Services.AddValidatorsFromAssembly(assembly);
 
+var dbConnectionString = builder.Configuration.GetConnectionString("Database")!;
+
 builder.Services.AddMarten(opts =>
 {
-    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Connection(dbConnectionString);
 }).UseLightweightSessions();
 
 if (builder.Environment.IsDevelopment())
     builder.Services.InitializeMartenWith<CatalogInitialData>();
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(dbConnectionString);
 
 var app = builder.Build();
 
@@ -34,5 +41,11 @@ var app = builder.Build();
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
