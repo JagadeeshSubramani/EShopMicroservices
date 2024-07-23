55a051b2fad11f810ce121308f2f51b9985756ec
Jagadeesh Subramani <jagadeesh.subramani@versent.com.au>
2024-07-23 12:58:38 +1000

    Added pagination to get products

--- eshop-microservices/src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -7,3 +7,4 @@ global using Marten;
 global using FluentValidation;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions.Handler;
+global using Marten.Pagination;
--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,6 +1,6 @@
 ﻿namespace Catalog.API.Products.GetProducts
 {
-    public record GetProductsQuery : IQuery<GetProductsResult>;
+    public record GetProductsQuery(int PageNumber = 1, int PageSize = 10) : IQuery<GetProductsResult>;
 
     public record GetProductsResult(IEnumerable<Product> Products);
 
@@ -8,7 +8,7 @@
     {
         public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {            
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            var products = await session.Query<Product>().ToPagedListAsync(query.PageNumber, query.PageSize, cancellationToken);
 
             return new GetProductsResult(products);
         }
